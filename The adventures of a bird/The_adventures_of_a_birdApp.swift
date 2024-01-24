14690ad9376b10b1daecaa77e100a502daadf7c0
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-01-24 13:38:50 +0300

    Initial Commit

---
+++ The adventures of a bird/The_adventures_of_a_birdApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_adventures_of_a_birdApp.swift
+//  The adventures of a bird
+//
+//  Created by Роман on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct The_adventures_of_a_birdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
